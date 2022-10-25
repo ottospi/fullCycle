649e690b0b21c5ad23b1876ca0ec28420f509afa
Otto Spigariol <ottospigariol@Ottos-MacBook-Pro.local>
2022-10-25 00:09:50 -0300

    adding kafka prod and consumer

--- simulator/main.go
+++ simulator/main.go
@@ -3,7 +3,15 @@ package main
 import (
 	"fmt"
 	route2 "github.com/code/edu/imersaofsfc2-simulator/application/route"
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+}
 
 func main() {
 	route :=  route2.Route{
